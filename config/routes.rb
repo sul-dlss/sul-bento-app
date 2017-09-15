fcea2cac3d534f8a627ee6a5176e0a893ba7cdb2
Jack Reed <phillipjreed@gmail.com>
2017-09-15 10:05:26 -0600

    make the application layout the global one

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   mount QuickSearch::Engine => "/"
+
+  root to: 'quick_search/search#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
