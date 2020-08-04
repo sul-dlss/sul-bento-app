da93f71b8426e41abd7be80035ddb5f3fc547db1
Chris Beer <cabeer@stanford.edu>
2020-08-04 10:12:13 -0700

    Use sul_styles

--- Gemfile
+++ Gemfile
@@ -103,3 +103,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'okcomputer'
+gem  'sul_styles', path: '/Users/cabeer/Projects/sul/sul_styles'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def body_tag_attributes
+    {
+      data: {
+        'quicksearch-ga-query': strip_tags(@log_query)
+      }
+    }
+  end
 end
